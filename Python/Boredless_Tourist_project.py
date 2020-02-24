402cf95249b7fa9ade99ddeea5d21dd427706367
Nik Fernandez <niklas_fernandez@comcast.net>
2020-02-23 20:44:39 -0600

    Added interest finder logic

--- Python/Boredless_Tourist_project.py
+++ Python/Boredless_Tourist_project.py
@@ -1,6 +1,7 @@
 # load in our data
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+attractions = [[], [], [], [], []]
 
 # grab the index that pertains to the specified destination
 def get_destination_index(destination):
@@ -13,7 +14,7 @@ def get_destination_index(destination):
 
     return destination_index
 # test the above method
-print(get_destination_index("Paris, France"))
+#print(get_destination_index("Paris, France"))
 
 # get the location of any given traveler
 def get_traveler_location(traveler):
@@ -21,10 +22,8 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
-test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
-
-attractions = [[], [], [], [], []]
+#test_destination_index = get_traveler_location(test_traveler)
+#print(test_destination_index)
 
 # add an attraction to a destination
 def add_attraction(destination, attraction):
@@ -41,6 +40,47 @@ def add_attraction(destination, attraction):
         # simply return
         return
 
-# add a sample attraction -- passed in a location and an attraction with a description
+# add sample attractions -- passed in a location and an attraction with a description
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
-print(attractions)
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+#print(attractions)
+
+# finds a destinatio nthe pertains to a traveler's interests
+def find_attractions(destination, interests):
+    # find our destination to search our attractions
+    destination_index = get_destination_index(destination)
+    # we now can grab the attractions in that location
+    attractions_in_city = attractions[destination_index]
+
+    # this will be populated if any of our traveler's interests coincide
+    # with any of the descriptions from our attraction's list
+    attractions_with_interest = []
+    attraction_tags = []
+
+    # retrieve all tags
+    for possible_attraction in attractions_in_city:
+        attraction_tags.append(possible_attraction)
+        # check if each interest matches the current possible_attraction
+        for interest in interests:
+            for attraction_tag in attraction_tags:
+                # check the lists in each tag
+                for item in attraction_tag[1]:
+                    if item == interest:
+                        attractions_with_interest.append(attraction_tag[0])
+
+    # give the user the list
+    return attractions_with_interest
+
+
+# practice our above method -- does Los Angeles have an attraction for art?
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
