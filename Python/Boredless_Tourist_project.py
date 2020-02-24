7e63d550c66dd2345ace6c97c19e01d653d087e2
Nik Fernandez <niklas_fernandez@comcast.net>
2020-02-23 19:39:11 -0600

    Added more functionality. Up to 36/64

--- Python/Boredless_Tourist_project.py
+++ Python/Boredless_Tourist_project.py
@@ -1,8 +1,8 @@
+# load in our data
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
-
+# grab the index that pertains to the specified destination
 def get_destination_index(destination):
     destination_index = 0
 
@@ -12,10 +12,10 @@ def get_destination_index(destination):
             destination_index = index
 
     return destination_index
-
+# test the above method
 print(get_destination_index("Paris, France"))
 
-
+# get the location of any given traveler
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
@@ -26,13 +26,21 @@ print(test_destination_index)
 
 attractions = [[], [], [], [], []]
 
+# add an attraction to a destination
 def add_attraction(destination, attraction):
     try:
+        # grab the destination of the attraction
         destination_index = get_destination_index(destination)
+        # grab the list from attractions
+        attractions_for_destination = attractions[destination_index]
+        # we now want to add that attraction to our list
+        attractions_for_destination.append(attraction)
 
-        attractions_for_destination = attractions
-
-        return attractions_for_destination
+        return
     except ValueError:
         # simply return
         return
+
+# add a sample attraction -- passed in a location and an attraction with a description
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+print(attractions)
