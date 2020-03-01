f6591cda0710d1939b9be1f7ea556f349fa02e76
Nik Fernandez <niklas_fernandez@comcast.net>
2020-03-01 16:53:54 -0600

    Working on active project and removed old project.

--- Python/project_censor_dispenser/censor_dispenser.py
+++ Python/project_censor_dispenser/censor_dispenser.py
@@ -9,8 +9,6 @@ def censor_text_multiple(text):
     return text.lower().replace(proprietary_terms[0], '*****').replace(proprietary_terms[1], '*****').replace(proprietary_terms[2], '*****').replace(proprietary_terms[3], '*****').replace(proprietary_terms[4], '*****').replace(proprietary_terms[5], '*****').replace(proprietary_terms[6], '*****')
 
 # 4. Censor 'negative words', after two occurrences
-def censor_negative_words(text):
-    negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
 
 # These are the emails you will be censoring. The open() function is opening the text file that the emails are contained in and the .read() method is allowing us to save their contexts to the following variables:
@@ -28,3 +26,29 @@ print('\n\n')
 # 3. Cencsor email 2
 email_two_censored = censor_text_multiple(email_two)
 print(email_two_censored)
+
+print('\n\n')
+
+# 4. Tone down the negative words
+print('Censoring negative words: ')
+
+def censor_text_negative_words_multiple(text):
+  negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
+  # this string is meant for holding a modified version of itself; anytime we need to censor a word, we need to change the string somehow
+  string_to_modify = text
+  # circulate through every word in the list and check if it occurs more than once. If so, censor the word.
+  lst_of_strings = text.split(' ')
+  print('DEBUG: List of strings: ' + str(lst_of_strings))
+  for current_word in negative_words:
+    # go through all of the text
+    for val in lst_of_strings:
+      count = 0
+      if current_word == val.lower():
+        count += 1
+      if count > 1:
+        string_to_modify = text.replace(current_word, '******')
+
+  return(censor_text_multiple(string_to_modify))
+
+print(censor_text_negative_words_multiple(email_three))
